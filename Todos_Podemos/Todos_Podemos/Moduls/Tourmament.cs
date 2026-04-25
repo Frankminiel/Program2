c8148bd037b0b82be6587273c26894348e4a9a98
Frankminiel <167045637+Frankminiel@users.noreply.github.com>
2026-04-25 12:41:32 -0400

    Delete Todos_Podemos directory

--- Todos_Podemos/Todos_Podemos/Moduls/Tourmament.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Todos_Podemos.Moduls
-{
-    public class Tournament
-    {
-        public int? Id { get; set; }
-        public string Name { get; set; }
-        public DateTime Date { get; set; }
-    }
-}
--- Todos_Podemos/todos_podemos.api/Models/Tourmament.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace todospodemos.api.Models
-{
-    public class Tournament
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime Date { get; set; }
-    }
-}
